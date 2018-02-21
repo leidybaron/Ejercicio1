4220f24206ad4e2fd851714d6843a5beb59980ce
Estudiantes <Estudiantes@Labing70209>
2018-02-21 17:41:58 -0500

    Ejercicio Parqueadero1

---
+++ Vehiculos/src/Parqueadero/Parqueadero.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parqueadero;
+
+import vehiculos.Moto;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Parqueadero {
+    
+    public static void main(String[] args)
+    {
+        FichaVehiculo miFicha;
+        miFicha=new FichaVehiculo();
+        
+        Moto miMoto=new Moto();
+        
+        
+        
+        
+    }
+    
+}
